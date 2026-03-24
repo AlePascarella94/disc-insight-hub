1a10f091bfca7bf16b3772639e581ea8494f5e32
AlePascarella94 <prgpascarella@gmail.com>
2026-03-24 18:40:03 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "./", // <--- ADICIONE ESTA LINHA AQUI
+  base: "./",
   server: {
     host: "::",
     port: 8080,
